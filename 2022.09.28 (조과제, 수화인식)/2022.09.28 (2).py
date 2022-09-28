aa483cf5202e207af33d428762e83a5391fa1f71
Kim YoungWon <57243486+Sodness@users.noreply.github.com>
2022-09-28 17:54:49 +0900

    Add files via upload

---
+++ 2022.09.28 (조과제, 수화인식)/2022.09.28 (2).py
@@ -0,0 +1,39 @@
+# flask + mediapipe 손가락 인식 + HTML,CSS,Javascript
+
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__)
+
+@app.route("/")
+def mediapipe():
+    return render_template('hands.html')
+
+if __name__ == '__main__':
+    app.run(port='5100', debug=True)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
